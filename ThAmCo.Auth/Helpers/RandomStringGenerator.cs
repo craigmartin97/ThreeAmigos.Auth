cbadb9c3d39b360ddec7551ce7ee791e96d8f871
Craig Martin <c.martin0297@gmail.com>
2019-12-04 21:43:07 +0000

    Update

---
+++ ThAmCo.Auth/Helpers/RandomStringGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace ThAmCo.Auth.Helpers
+{
+    public class RandomStringGenerator
+    {
+        /// <summary>
+        /// Generate a random stirng for a temporary access token
+        /// </summary>
+        /// <returns>A randomly generated string</returns>
+        public string GenerateRandomString(int limit, string text = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+        => new string(Enumerable.Repeat(text, limit).Select(s => s[new Random().Next(s.Length)]).ToArray());
+    }
+}
