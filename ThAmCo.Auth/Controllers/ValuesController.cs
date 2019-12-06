65ed73f48c524c949eaa434f3477374124f1e789
Craig Martin <c.martin0297@gmail.com>
2019-12-06 22:09:54 +0000

    Edited values controller

--- ThAmCo.Auth/Controllers/ValuesController.cs
+++ ThAmCo.Auth/Controllers/ValuesController.cs
@@ -7,12 +7,11 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ThAmCo.Auth.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
         // GET api/values
-        [HttpGet]
+        [HttpGet("api/values")]
         public ActionResult<IEnumerable<string>> Get()
         {
             return new string[] { "value1", "value2" };
